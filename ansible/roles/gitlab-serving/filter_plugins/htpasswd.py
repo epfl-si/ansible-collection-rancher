2b9b47873c612c7ac86fd1b05b6b3a135b6d1ca4
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-08-24 17:50:23 +0200

    [fix] Deterministic password hashing
    - Add an optional parameter to the htpasswd() Jinja filter, which
    makes it deterministic when used
    - Do our best to feed the finicky bcrypt functions with a
    deterministic salt derived therefrom

--- ansible/roles/gitlab-serving/filter_plugins/htpasswd.py
+++ ansible/roles/gitlab-serving/filter_plugins/htpasswd.py
@@ -1,21 +1,32 @@
-# Transform clear text list of username:password into corresponding entry
-# suitable for traefik traefik.frontend.auth.basic label
-# Keeps the salt in a tmp file so that runs are more or less idempotents.
+# Create the right-hand side of an htpasswd line.
+from bcrypt import gensalt, hashpw, kdf
+from base64 import b64encode
 
-from bcrypt import gensalt, hashpw
+KDF_COST_FACTOR = 50
 
-# Oops... just found that there is a standard hash filter in ansible
-#         well in the end it does not simply much   :p
-# from ansible.plugins.filter.core import get_encrypted_password
-# from ansible.utils.encrypt import random_salt
+def b(s):
+    return bytes(s, 'utf-8')
 
-def htpasswd(passwd):
-  salt = gensalt()
+def s(b):
+    return str(b, 'utf-8')
 
-  ep = hashpw(bytes(passwd, 'utf-8'), salt)
-  ep = str(ep, 'utf-8')
-  return ep
+def htpasswd(passwd, salt_seed = None):
+    salt = gensalt()
+    if salt_seed is not None:
+
+        salt_pieces = s(salt).split('$')
+
+        # https://github.com/ansible/ansible/issues/36129#issuecomment-658832705
+        salt_last_bits = kdf(b(passwd), b(salt_seed),
+                             18,   # Resulting in a 24-character long string
+                             KDF_COST_FACTOR)
+        salt_pieces[3] = s(b64encode(salt_last_bits, b'./')[:21]
+                           + b'.')  # Losing 2 bits of entropy, so sue me
+
+        salt = b('$'.join(salt_pieces))
+
+    return s(hashpw(b(passwd), salt))
 
 class FilterModule(object):
-  def filters(self):
-    return {'htpasswd': htpasswd }
+    def filters(self):
+        return {'htpasswd': htpasswd }
