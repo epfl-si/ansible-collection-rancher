3ae9cd2042dbb6e2e8df544591c252892f7b16db
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-08-24 19:18:20 +0200

    [WIP][refactor] Serving infrastructure: databases
    - Create scripts that are used for staging (as opposed to embedding
    them as flat file in the Git repo)
    
    - Piece out the remainder of docker-compose.yml into `docker_compose:`
    tasks
    
    - Still WIP: there is a PostgreSQL password floating about; should
    move to Keybase

--- staging/log-queries.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Found on https://github.com/Roconda/docker-postgres-logging/blob/master/Dockerfile
-set -e
-
-sed -ri "s/^#log_statement.*/log_statement = 'all'/g" /var/lib/postgresql/data/postgresql.conf
