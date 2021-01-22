6e06d5101c3849032aa76868dcc98c32d5e1b9e9
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-01-22 16:34:56 +0100

    [refactor] Jinja filters without the OO nonsense

--- ansible/roles/gitlab-k8s/filter_plugins/adhoc.py
+++ ansible/roles/gitlab-k8s/filter_plugins/adhoc.py
@@ -1,15 +1,17 @@
-"""Ad-hoc helpers that are hard to write in pure Jinja."""
+"""Ad-hoc helpers that are hard or impossible to write in pure Jinja."""
 
 class FilterModule(object):
     def filters(self):
         return {
-            'keys': self.keys,
-            'as_volume_mounts': self.as_volume_mounts
+            'as_volume_mounts': as_volume_mounts
+            'keys': keys,
         }
-    def as_volume_mounts(self, filenames, secret_ref_name, mount_at):
-        return [dict(name=secret_ref_name,
-                     mountPath="%s/%s" % (mount_at, filename),
-                     subPath=filename)
-                for filename in filenames]
-    def keys(self, d):
-        return d.keys()
+
+def as_volume_mounts(filenames, secret_ref_name, mount_at):
+    return [dict(name=secret_ref_name,
+                 mountPath="%s/%s" % (mount_at, filename),
+                 subPath=filename)
+            for filename in filenames]
+
+def keys(d):
+    return d.keys()
--- ansible/roles/gitlab-k8s/filter_plugins/base64.py
+++ ansible/roles/gitlab-k8s/filter_plugins/base64.py
@@ -5,11 +5,12 @@ from base64 import b64encode
 class FilterModule(object):
     def filters(self):
         return {
-            'base64_values': self.base64_values
+            'base64_values': base64_values
         }
-    def base64_values(self, d):
-        return dict((k, b64encode(pry_out_bytes(v)).decode("ascii"))
-                    for k, v in d.items())
+
+def base64_values(d):
+    return dict((k, b64encode(pry_out_bytes(v)).decode("ascii"))
+                for k, v in d.items())
 
 def pry_out_bytes(soi_disant_unicode_string_a_la_Python):
     """YA RLY.
