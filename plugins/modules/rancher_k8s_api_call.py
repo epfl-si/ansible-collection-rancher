b77ae5f5e523dad1ef6e2c17d297a450981f7f16
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2024-09-17 11:55:57 +0200

    [feature] `epfl_si.rancher.rancher_k8s_api_call` task, with documentation

---
+++ plugins/modules/rancher_k8s_api_call.py
@@ -0,0 +1,39 @@
+from functools import cached_property
+import json
+
+from ansible_collections.kubernetes.core.plugins.module_utils.k8s.core import AnsibleK8SModule
+from ansible_collections.kubernetes.core.plugins.module_utils.k8s.client import get_api_client
+
+class RancherAPICall:
+    """Implementation for `rancher_k8s_api_call` Ansible tasks."""
+    module_args = dict(
+        method=dict(type='str'),
+        uri=dict(type='str'),
+        body=dict(type='dict'))
+
+    @cached_property
+    def k8s_client (self):
+        client = get_api_client(self.module)
+        return client.client
+
+    @cached_property
+    def module (self):
+        return AnsibleK8SModule(
+            argument_spec=self.module_args)
+
+    def run (self):
+        # https://stackoverflow.com/a/63747147
+        (data, status, headers) = self.k8s_client.client.call_api(
+            self.module.params['uri'],
+            self.module.params['method'],
+            auth_settings = ['BearerToken'],
+            response_type="object",
+            body=self.module.params['body'])
+
+        self.module.exit_json(
+            changed=True,
+            api_response=data)
+
+
+if __name__ == '__main__':
+    RancherAPICall().run()
