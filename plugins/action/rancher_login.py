2a3fc8839e866ac79e48d237a4b498622de1f829
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-01-15 17:46:56 +0100

    [doc][refactor] the documentation of the `rancher_login` action plugin
    It seems like documentation for action plugins is not supported, like,
    at all.
    
    - Put it in a dud module with the same name. (There is
    [precedent](https://github.com/ansible/ansible/blob/devel/lib/ansible/modules/template.py)
    for that in the source code of Ansible itself, for the `template`
    action plugin. Plus, it works i.e. Ansible picks up the action plugin
    over the module with the same name.)
    - As there isn't a supported way to document Ansible variables,
    fashion some really wonky definition list out of whatever YAML
    `ansible-doc` will grok.

--- plugins/action/rancher_login.py
+++ plugins/action/rancher_login.py
@@ -12,58 +12,9 @@ from ansible_collections.epfl_si.rancher.plugins.module_utils.rancher_actions im
 class RancherLoginAction (ActionBase, RancherActionMixin):
     """Download a Kubeconfig file from the rancher back-end.
 
-    1. connect over ssh to the node where the Rancher front-end is running,
-    2. create an authentication token as a `kind: Token` object with a very short expiration time,
-    3. request the “child” cluster's kubeconfig from the Rancher backend (like the Web UI would),
-       using the token for authentication,
-    4. present the result of same as the `.kubeconfig` property of the task result.
-
-    ⚠ This action *does not* check any pre-existing credentials; as a
-    consequence, it always satisfies the `is changed` Jinja predicate
-    (a.k.a. is “always yellow” in the Ansible UI). Likewise, this
-    action doesn't store the retrieved credentials in a kubeconfig
-    file. It behooves the calling role or playbook to take care of
-    both.
-
-    Recognized task args:
-        cluster_name      The name of the cluster (the one you see in the
-                          “name” column on the Rancher dashboard). Defaults
-                          to the `ansible_rancher_cluster_name` variable,
-                          if set.
-
-    Ansible variables used:
-        ansible_rancher_cluster_name   The default for the `cluster_name`
-                                       task argument
-
-        ansible_rancher_url            The hostname portion of that URL
-                                       indicates the host to ssh into to
-                                       obtain credentials
-
-        ansible_rancher_ca_cert
-        ansible_rancher_validate_certs The usual parameters for
-                                       server-to-client TLS authentication,
-                                       shared with other tasks in this Ansible
-                                       collection. At least one of these
-                                       must be set. `ansible_rancher_ca_cert`,
-                                       if set, must be a string in multiline
-                                       PEM format.
-                                       `ansible_rancher_validate_certs` is
-                                       a Boolean.
-
-        ansible_rancher_username       The Rancher username that the
-                                       retrieved credentials will belong to;
-                                       defaults to “admin”
-
-        ansible_rancher_token_stem     The prefix of the short-lived bearer
-                                       token that `epfl_si.rancher.rancher_login`
-                                       creates for itself in order to
-                                       download the (longer-lived)
-                                       credentials in kubeconfig
-                                       format. Visible forever (we
-                                       think ?) with `kubectl get
-                                       token` in the `local` cluster,
-                                       even though they expire after
-                                       two minutes.
+    See operation details and Ansible-level documentation in
+    ../modules/rancher_login.py which only exists for documentation
+    purposes.
     """
     @AnsibleActions.run_method
     def run (self, args, ansible_api):
---
+++ plugins/modules/rancher_login.py
@@ -0,0 +1,113 @@
+# This file is here for ansible-doc purposes **only**. The actual
+# implementation is in ../action/rancher_login.py as an action plugin
+# (i.e. it runs on the Ansible controller.)
+
+DOCUMENTATION = r'''
+---
+module: rancher_login
+short_description: Log in to Rancher
+description:
+- This module is implemented entirely as an B(action plugin), that
+  runs on the Ansible controller.
+
+- This action plugin retrieves Kubernetes-style credentials (i.e.
+  C(kubeconfig) files) from the Rancher back-end. It B(does not)
+  revalidate pre-existing credentials, or store them to disk. It
+  behooves the calling role or playbook to take care of both; see
+  EXAMPLES.
+
+- >
+  This action plugin first connects over ssh to the node where the
+  Rancher front-end is running (as deduced from the
+  C(ansible_rancher_url) variable). It then generates an
+  authentication token as a C(kind: Token) object with a very short
+  expiration time if required; and finally it uses that to request the
+  “child” cluster's kubeconfig from the Rancher backend, like the Web
+  UI would.
+
+- 💡 This action plugin obtains credentials over ssh,
+  regardless of your C(ansible_connection) setting; and you should
+  therefore set either the C(ansible_user) or the C(ansible_ssh_user)
+  variable to specify the remote username to connect as (even though
+  the rest of your playbook may not make use of that variable).
+
+- "This action plugin reads from the following Ansible variables:"
+
+- C(ansible_rancher_cluster_name)
+- The default for the C(cluster_name) task argument
+
+- C(ansible_rancher_url)
+- The hostname portion of that URL
+  indicates the host to ssh into to
+  obtain credentials
+
+- C(ansible_rancher_validate_certs)
+- (Boolean) Whether to authenticate the TLS server. Setting this to false B(is insecure).
+  At least one of C(ansible_rancher_validate_certs) or C(ansible_rancher_ca_cert) must be set.
+
+- C(ansible_rancher_ca_cert)
+- The CA certificate to validate the Rancher TLS server, as a string in multiline
+  PEM format.
+
+- C(ansible_rancher_username)
+- The Rancher username that the
+  retrieved credentials will belong to;
+  defaults to “admin”
+
+- C(ansible_rancher_token_stem)
+- The prefix of the short-lived bearer token that this action plugin
+  creates for itself in order to download the (longer-lived)
+  credentials in kubeconfig format. The tokens remain visible forever
+  (we think ?) with C(kubectl get token) in the C(local) cluster, even
+  though they expire after two minutes.
+
+options:
+  cluster_name:
+    description: >
+      The name of the cluster (the one you see in the
+      “name” column on the Rancher dashboard). Defaults
+      to the value of the C(ansible_rancher_cluster_name)
+      variable.
+
+version_added: 0.2.1
+
+'''
+
+RETURN = r'''
+changed:
+  type: bool
+  description: True (“yellow”) iff creating a new C(Token) Kubernetes object
+               was necessary to retrieve the credentials. That is,
+               re-using an existing C(Token) results in unchanged (”green”)
+               Ansible outcome; or to put it otherwise, the part where we fetch
+               the Kubeconfig “doesn't count” for green / yellow.
+
+kubeconfig:
+  description: The contents of the C(kubeconfig) file downloaded from the Rancher backend,
+               as an unparsed, multiline YAML string.
+  type: str
+'''
+
+EXAMPLES = r'''
+- changed_when: false
+  shell:
+    cmd: |
+      if KUBECONFIG="$K8S_AUTH_KUBECONFIG" kubectl get pods ; then
+        echo "CREDENTIALS_ARE_STILL_VALID"
+        exit 0
+      fi
+  register: _credentials_check
+
+- when: >-
+     "CREDENTIALS_ARE_STILL_VALID" not in _credentials_check.stdout
+  epfl_si.rancher.rancher_login: {}
+  register: _rancher_login
+
+- when: >-
+     "CREDENTIALS_ARE_STILL_VALID" not in _credentials_check.stdout
+  copy:
+    dest: >-
+      {{ lookup("env", "K8S_AUTH_KUBECONFIG") }}
+    content: >-
+      {{ _rancher_login.kubeconfig }}
+'''
