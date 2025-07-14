8eb77529b95e0cdd7e90a239e548e7aada91922c
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-07-14 18:11:16 +0200

    [unfeature] Pretty much... everything but Rancher
    - `roles/gitlab-k8s` (was previously for `ic-gitlab.epfl.ch`)
    - `roles/gitlab-traefik` (where we are going, we won't need to manage
      Træfik; at least not directly)
    - `roles/gitlab-serving`, `vars/docker-networking.yml`
    - Salvage `roles/gitlab-k8s/vars/ssh-secrets.yml` into and hoist it to
      top-level `vars/`

--- roles/gitlab-serving/filter_plugins/iteritems.py
+++
@@ -1,7 +0,0 @@
-from ansible.module_utils import six
-
-class FilterModule(object):
-    def filters(self):
-        return {'iteritems': self.iteritems }
-    def iteritems(self, dict):
-        return [{'k': k, 'v': v} for k, v in six.iteritems(dict)]
