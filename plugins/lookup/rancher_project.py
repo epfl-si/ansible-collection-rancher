076234add6e597e9d4e013db3e43613a0a26bc28
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2026-01-21 18:27:15 +0100

    [refactor] `rancher_project` lookup in terms of `epfl_si.k8s`

--- plugins/lookup/rancher_project.py
+++ plugins/lookup/rancher_project.py
@@ -28,32 +28,39 @@ EXAMPLES = '''
 '''
 
 from ansible.plugins.lookup import LookupBase
-from ansible_collections.kubernetes.core.plugins.module_utils.k8s.client import get_api_client
+from ansible_collections.epfl_si.k8s.plugins.lookup.k8s import LookupModule as K8sLookup
 
-class LookupModule(LookupBase):
-    def _get_custom_resource (self, client, kind, api_version):
-        return client.resource(kind, api_version).get().items
+class LookupModule (LookupBase):
+    def _get_custom_resource (self, kind, api_version):
+        return K8sLookup(self._loader, self._templar).run(
+            [],
+            variables=self._variables,
+            api_version=api_version, kind=kind,
+            **self._kwargs)
 
-    def _get_clusters (self, client):
-        return self._get_custom_resource(client, 'cluster', 'provisioning.cattle.io/v1')
+    @property
+    def _all_clusters (self):
+        return self._get_custom_resource('cluster', 'provisioning.cattle.io/v1')
 
-    def _get_projects (self, client):
-        return self._get_custom_resource(client, 'project', 'management.cattle.io/v3')
+    @property
+    def _all_projects (self):
+        return self._get_custom_resource('project', 'management.cattle.io/v3')
 
-    def run (self, terms, variables=None, display_name=None, **client_kwargs):
-        client = get_api_client(**client_kwargs)
+    def run (self, terms, variables=None, display_name=None, **kwargs):
+        self._variables = variables
+        self._kwargs = kwargs
 
         this_cluster_name = variables["ansible_rancher_cluster_name"]
         [this_cluster_id] = (
-            c.status.clusterName for c in self._get_clusters(client)
-            if c.metadata.name == this_cluster_name)
+            c["status"]["clusterName"] for c in self._all_clusters
+            if c["metadata"]["name"] == this_cluster_name)
         assert this_cluster_id is not None
 
-        projects = [p for p in self._get_projects(client)
-                    if p.spec.clusterName == this_cluster_id]
+        projects = [p for p in self._all_projects
+                    if p["spec"]["clusterName"] == this_cluster_id]
 
         if display_name is not None:
             projects = [p for p in projects
-                        if p.spec.displayName == display_name]
+                        if p["spec"]["displayName"] == display_name]
 
         return projects
