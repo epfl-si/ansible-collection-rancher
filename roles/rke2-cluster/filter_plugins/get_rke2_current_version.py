e568649d9507a105f61ca0be3645d8836078e046
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2026-02-25 15:03:40 +0100

    [rewrite] `get_rke2_current_version` to fix yet another Pythonic train wreck
    - Get rid of `urllib`, so that we won't be following redirects anymore
    - Explicitly load certs out of `requests.utils.DEFAULT_CA_BUNDLE_PATH`, because for some reason `urllib` doesn't
    - Make dependency on `requests` module explicit

--- roles/rke2-cluster/filter_plugins/get_rke2_current_version.py
+++ roles/rke2-cluster/filter_plugins/get_rke2_current_version.py
@@ -1,6 +1,41 @@
 """Obtain the current (latest) version in an RKE2 release channel."""
 
-from urllib.request import urlopen
+import ssl
+import http.client
+from requests.utils import DEFAULT_CA_BUNDLE_PATH
+
+from urllib.parse import urlparse
+
+
+class HTTPStatusError (Exception):
+    pass
+
+
+def get_location_header (url):
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.check_hostname = True
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations(DEFAULT_CA_BUNDLE_PATH)
+
+    parsed = urlparse(url)
+    port = parsed.port or 443
+    path = parsed.path or "/"
+    if parsed.query:
+        path += "?" + parsed.query
+
+    conn = http.client.HTTPSConnection(
+        parsed.hostname,
+        port=port,
+        context=context)
+
+    conn.request("GET", path)
+    response = conn.getresponse()
+
+    status = response.status
+    if status < 300 or status > 399:
+        raise HTTPStatusError("Unexpected status %d at %s" % (status, url))
+
+    return response.getheader("Location")
 
 class FilterModule(object):
     def filters(self):
@@ -9,7 +44,5 @@ class FilterModule(object):
         }
 
     def get_rke2_current_version(self, channel_url):
-        with urlopen(channel_url) as response:
-            # You really want to *not* follow URLs. But for that, you
-            # would need a HorseFactory or something.
-            return response.geturl().split("/")[-1]
+        location = get_location_header(channel_url)
+        return location.split("/")[-1]
